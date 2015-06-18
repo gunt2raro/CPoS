1f18cf22a70ecbf8cc2ba0e8868cb3886f8ccc14
gunt2raro <gunt_raro@hotmail.com>
2015-06-18 04:27:07 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="js/script.js"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+
+    <title>CPoS</title>
+
+  </head>
+
+  <body>
+    <header class="page-header">
+
+      <h1>CPoS</h1>
+
+    </header>
+
+    <div class="container">
+        Here goes everything!!
+    </div>
+
+    <footer class="footer">
+      <span>Stoke Travel... All rights reserved...</span>
+    </footer>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+  </body>
+
+</html>
+
